8436ce5601649c08cbc23fa6942d78361b9b2569
Tilo Gaulke <tilo.gaulke@me.com>
2026-02-02 16:46:43 +0100

    change makefile to prepare xchess and new test

---
+++ test_if_placed_with_xchess/aie2.py
@@ -0,0 +1,141 @@
+from pkgutil import extend_path
+
+import numpy as np
+import sys
+import aie.utils.trace as trace_utils
+
+from aie.dialects.aie import *
+from aie.dialects.aiex import *
+from aie.helpers.dialects.scf import _for as range_, if_, else_
+from aie.extras.context import mlir_mod_ctx
+from setuptools.archive_util import extraction_drivers
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "npu":
+        dev = AIEDevice.npu1
+    elif sys.argv[1] == "npu2":
+        dev = AIEDevice.npu2
+    else:
+        raise ValueError("[ERROR] Device name {} is unknown".format(sys.argv[1]))
+
+def external_mem_to_core():
+    with mlir_mod_ctx() as ctx:
+
+        @device(dev)
+        def device_body():
+            elements = 64
+            tile_ty_size = 1
+            iters = elements // tile_ty_size
+
+
+            tile_ty = np.ndarray[(tile_ty_size,), np.dtype[np.int32]]
+            #trace_size = 8192
+
+            # External, binary kernel definition
+            vector_plus_one = external_func(
+                "vector_plus_one",
+                inputs=[tile_ty, tile_ty, np.int32]
+            )
+
+            # Tile declarations
+            ShimTile00 = tile(0, 0)
+            ShimTile10 = tile(1, 0)
+            #ShimTile20 = tile(2, 0)
+            MemTile01 = tile(0, 1)
+            MemTile11 = tile(1, 1)
+            ComputeTile02 = tile(0, 2)
+            ComputeTile12 = tile(1, 2)
+
+            # AIE-array data movement with object fifos
+            # Input
+            of_in = object_fifo("in", ShimTile00, MemTile01, 2, tile_ty)
+            of_in1 = object_fifo("in1", MemTile01, ComputeTile02, 2, tile_ty)
+            object_fifo_link(of_in, of_in1)
+
+            # Computation teil connector
+            of_02_12 = object_fifo("02to12", ComputeTile02, ComputeTile12, 2, tile_ty)
+
+            # Output
+            of_out1 = object_fifo("out1", ComputeTile12, MemTile11, 2, tile_ty)
+            of_out = object_fifo("out", MemTile11, ShimTile10, 2, tile_ty)
+            object_fifo_link(of_out1, of_out)
+
+            of_out1_odd = object_fifo("outodd", ComputeTile12, MemTile11, 2, tile_ty)
+            of_out_odd = object_fifo("odd", MemTile11, ShimTile10, 2, tile_ty)
+            object_fifo_link(of_out1_odd, of_out_odd)
+
+            # Set up compute tiles
+            # Compute tile
+            @core(ComputeTile02, "vector_operators.o")
+            def core_body_02():
+                # Effective while(1)
+                for _ in range_(iters):
+                    elem_in = of_in1.acquire(ObjectFifoPort.Consume, 1)
+                    elem_out = of_02_12.acquire(ObjectFifoPort.Produce, 1)
+                    call(vector_plus_one, [elem_in, elem_out, tile_ty_size])
+                    of_in1.release(ObjectFifoPort.Consume, 1)
+                    of_02_12.release(ObjectFifoPort.Produce, 1)
+
+
+
+            @core(ComputeTile12, "vector_operators.o")
+            def core_body_12():
+                # Effective while(1)
+                for _ in range_(iters):
+                    elem_in = of_02_12.acquire(ObjectFifoPort.Consume, 1)
+                    #elem_out = of_out1.acquire(ObjectFifoPort.Produce, 1)
+                    #elem_out[0]=elem_in[0]
+                    #elemOut_even = of_out1.acquire(ObjectFifoPort.Produce, 1)
+                    with if_(elem_in[0] % 2 == 0, hasElse=True) as if_op:
+                        # pass
+                        elemOut_even = of_out1.acquire(ObjectFifoPort.Produce, 1)
+                        elemOut_even[0] = elem_in[0]
+                        of_out1.release(ObjectFifoPort.Produce, 1)
+                    with else_(if_op):
+                        pass
+                        elemOut_odd = of_out1_odd.acquire(ObjectFifoPort.Produce, 1)
+                        elemOut_odd[0] = elem_in[0]
+                        of_out1_odd.release(ObjectFifoPort.Produce, 1)
+                    #of_out1.release(ObjectFifoPort.Produce, 1)
+                    of_02_12.release(ObjectFifoPort.Consume, 1)
+                    #of_out1.release(ObjectFifoPort.Produce, 1)
+
+            # To/from AIE-array data movement
+            data_ty = np.ndarray[(elements,), np.dtype[np.int32]]
+
+            #tiles_to_trace = [ComputeTile02, MemTile01, ShimTile00]
+            #if trace_size > 0:
+            #    trace_utils.configure_packet_tracing_flow(tiles_to_trace, ShimTile20)
+
+            @runtime_sequence(data_ty, data_ty,data_ty)
+            def sequence(inTensor,outOddTensor, outTensor,):
+                #if trace_size > 0:
+                #    trace_utils.configure_packet_tracing_aie2(
+                #        tiles_to_trace=tiles_to_trace,
+                #        shim=ShimTile20,
+                #        trace_size=trace_size,
+                #    )
+                npu_dma_memcpy_nd(
+                    metadata=of_in, bd_id=1, mem=inTensor, sizes=[1, 1, 1, elements]
+                )
+
+
+                npu_dma_memcpy_nd(
+                    metadata=of_out_odd, bd_id=0, mem=outOddTensor, sizes=[1, 1, 1, 32]
+                )
+
+                npu_dma_memcpy_nd(
+                    metadata=of_out, bd_id=2, mem=outTensor, sizes=[1, 1, 1, 32]
+                )
+                # of_out will only complete after of_in completes, so we can just wait on of_out instead of both
+                dma_wait(of_out)
+                #trace_utils.gen_trace_done_aie2(ShimTile20)
+
+    res = ctx.module.operation.verify()
+    if res == True:
+        print(ctx.module)
+    else:
+        print(res)
+
+
+external_mem_to_core()
---
+++ test_local_mem_join/test.cpp
@@ -0,0 +1,206 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "cxxopts.hpp"
+#include "test_utils.h"
+#include "xrt/xrt_bo.h"
+#include "xrt/xrt_device.h"
+#include "xrt/xrt_kernel.h"
+
+#ifndef DATATYPES_USING_DEFINED
+#define DATATYPES_USING_DEFINED
+using DATATYPE = std::uint32_t;
+#endif
+
+int main(int argc, const char *argv[]) {
+  // Program arguments parsing
+  cxxopts::Options options("section-3");
+  test_utils::add_default_options(options);
+
+  cxxopts::ParseResult vm;
+  test_utils::parse_options(argc, argv, options, vm);
+  int verbosity = vm["verbosity"].as<int>();
+  int n_iterations = vm["iters"].as<int>();
+  int n_warmup_iterations = vm["warmup"].as<int>();
+  int trace_size = vm["trace_sz"].as<int>();
+  std::string trace_file = vm["trace_file"].as<std::string>();
+
+  // Declaring design constants
+  constexpr bool VERIFY = true;
+  constexpr int IN_SIZE = 64;
+  constexpr int OUT_SIZE = IN_SIZE;
+
+  // Load instruction sequence
+  std::vector<uint32_t> instr_v =
+      test_utils::load_instr_binary(vm["instr"].as<std::string>());
+
+  if (verbosity >= 1)
+    std::cout << "Sequence instr count: " << instr_v.size() << "\n";
+
+  // Start the XRT context and load the kernel
+  xrt::device device;
+  xrt::kernel kernel;
+
+  test_utils::init_xrt_load_kernel(device, kernel, verbosity,
+                                   vm["xclbin"].as<std::string>(),
+                                   vm["kernel"].as<std::string>());
+
+  // set up the buffer objects
+  auto bo_instr = xrt::bo(device, instr_v.size() * sizeof(int),
+                          XCL_BO_FLAGS_CACHEABLE, kernel.group_id(1));
+  auto bo_inA = xrt::bo(device, IN_SIZE * sizeof(DATATYPE),
+                        XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(3));
+
+  auto bo_outOdd = xrt::bo(device, OUT_SIZE * sizeof(DATATYPE),
+                         XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(4));
+  auto bo_outC = xrt::bo(device, OUT_SIZE * sizeof(DATATYPE),
+                         XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(5));
+  int tmp_trace_size = (trace_size > 0) ? trace_size * 4 : 1;
+  auto bo_trace = xrt::bo(device, tmp_trace_size,
+						XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(7));
+
+  if (verbosity >= 1)
+    std::cout << "Writing data into buffer objects.\n";
+
+  // Copy instruction stream to xrt buffer object
+  void *bufInstr = bo_instr.map<void *>();
+  memcpy(bufInstr, instr_v.data(), instr_v.size() * sizeof(int));
+
+  // Initialize buffer bo_inA
+  DATATYPE *bufInA = bo_inA.map<DATATYPE *>();
+  for (int i = 0; i < IN_SIZE; i++)
+    bufInA[i] = i;
+
+  // Zero out buffer bo_outC
+  DATATYPE *bufOut = bo_outC.map<DATATYPE *>();
+  memset(bufOut, 0, OUT_SIZE * sizeof(DATATYPE));
+
+  DATATYPE *bufOutOdd = bo_outOdd.map<DATATYPE *>();
+  memset(bufOutOdd, 0, OUT_SIZE * sizeof(DATATYPE));
+
+
+  // Initialize buffer bo_trace
+  char *bufTrace = bo_trace.map<char *>();
+  memset(bufTrace, 0, trace_size);
+
+  // sync host to device memories
+  bo_instr.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_inA.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_outC.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_outOdd.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_trace.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+  // Execute the kernel and wait to finish
+  int errors = 0;
+  unsigned num_iter = n_iterations + n_warmup_iterations;
+  float npu_time_total = 0;
+  float npu_time_min = 9999999;
+  float npu_time_max = 0;
+  for (unsigned iter = 0; iter < num_iter; iter++) {
+    if (verbosity >= 1)
+      std::cout << "Running Kernel.\n";
+
+    auto start = std::chrono::high_resolution_clock::now();
+    unsigned int opcode = 3;
+    auto run =
+        kernel(opcode, bo_instr, instr_v.size(), bo_inA, bo_outC, bo_outOdd, 0, bo_trace);
+    run.wait();
+	bo_trace.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    // Sync device to host memories
+    bo_outC.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+    bo_outOdd.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+    // Accumulate run times
+    /* Warmup iterations do not count towards average runtime. */
+    if (iter < n_warmup_iterations) {
+      continue;
+    }
+    float npu_time =
+        std::chrono::duration_cast<std::chrono::microseconds>(stop - start)
+            .count();
+    std::cout << std::endl
+              << "NPU time: " << npu_time << "us."
+              << std::endl;
+    npu_time_total += npu_time;
+    npu_time_min = (npu_time < npu_time_min) ? npu_time : npu_time_min;
+    npu_time_max = (npu_time > npu_time_max) ? npu_time : npu_time_max;
+
+	// only output the trace with the first formal iteration
+	if (trace_size > 0 && iter == n_warmup_iterations) {
+		test_utils::write_out_trace((char *)bufTrace, trace_size, trace_file);
+	}
+
+    // Compare out to golden
+    if (verbosity >= 1) {
+      std::cout << "Verifying results ..." << std::endl;
+    }
+    for (uint32_t i = 0; i < IN_SIZE; i++) {
+      int32_t test = bufOut[i];
+      std::cout << test << " ";
+    }
+    std::cout  << "\n";
+    for (uint32_t i = 0; i < IN_SIZE; i++) {
+      int32_t test = bufOutOdd[i];
+      std::cout << test << " ";
+    }
+    std::cout << std::endl;
+    for (uint32_t i = 0; i < IN_SIZE; i++) {
+      int32_t ref = bufInA[i] + 2;
+      int32_t test = bufOut[i];
+      if (test != ref) {
+        if (verbosity >= 1)
+          std::cout << "Error in output " << test << " != " << ref << std::endl;
+        errors++;
+      } else {
+        if (verbosity >= 1)
+          std::cout << "Correct output " << test << " == " << ref << std::endl;
+      }
+    }
+  }
+
+  // print out profiling result
+  std::cout << std::endl
+          << "Number of iterations: " << n_iterations
+          << " (warmup iterations: " << n_warmup_iterations << ")"
+          << std::endl;
+
+  std::cout << std::endl
+            << "Avg NPU time: " << npu_time_total / n_iterations << "us."
+            << std::endl;
+
+
+  float macs = 0;
+  if (macs > 0)
+    std::cout << "Avg NPU gflops: "
+              << macs / (1000 * npu_time_total / n_iterations) << std::endl;
+
+  std::cout << std::endl
+            << "Min NPU time: " << npu_time_min << "us." << std::endl;
+  if (macs > 0)
+    std::cout << "Max NPU gflops: " << macs / (1000 * npu_time_min)
+              << std::endl;
+
+  std::cout << std::endl
+            << "Max NPU time: " << npu_time_max << "us." << std::endl;
+  if (macs > 0)
+    std::cout << "Min NPU gflops: " << macs / (1000 * npu_time_max)
+              << std::endl;
+
+
+  // Print Pass/Fail result of our test
+  if (!errors) {
+    std::cout << std::endl << "PASS!" << std::endl << std::endl;
+    return 0;
+  } else {
+    std::cout << std::endl
+              << errors << " mismatches." << std::endl
+              << std::endl;
+    std::cout << std::endl << "fail." << std::endl << std::endl;
+    return 1;
+  }
+}
