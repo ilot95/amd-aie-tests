47b0ac4fd0ec2df9c3e9fb0818d519cce8616fac
Tilo Gaulke <tilo.gaulke@me.com>
2026-03-02 15:27:58 +0100

    join working

---
+++ join_new/aie2.py
@@ -0,0 +1,208 @@
+from pkgutil import extend_path
+
+import numpy as np
+import sys
+import aie.utils.trace as trace_utils
+
+from aie.dialects.aie import *
+from aie.dialects.aiex import *
+from aie.helpers.dialects.scf import _for as range_, if_, else_
+from aie.extras.context import mlir_mod_ctx
+from setuptools.archive_util import extraction_drivers
+
+#use stderr so the mlir output does not break
+#These are don't have to be errors
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "npu":
+        dev = AIEDevice.npu1
+    elif sys.argv[1] == "npu2":
+        dev = AIEDevice.npu2
+    else:
+        raise ValueError("[ERROR] Device name {} is unknown".format(sys.argv[1]))
+
+trace_size = 0
+if len(sys.argv) > 2:
+    if sys.argv[2].isdigit():
+        trace_size = int(sys.argv[2])
+        eprint("[INFO] trace_size: {}".format(trace_size))
+    else:
+        eprint("[Info] sys.argv[2] (trace_size):{} is not a positive number falling back to trace_size = 0".format(sys.argv[2]))
+
+host_elements = 1024
+if len(sys.argv) > 3:
+    if sys.argv[3].isdigit():
+        host_elements = int(sys.argv[3])
+        eprint("[INFO] host_elements: {}".format(host_elements))
+    else:
+        eprint("[Info] sys.argv[3] (host_elements):{} is not a positive number falling back to host_elements = 1024".format(sys.argv[3]))
+
+
+
+def external_mem_to_core():
+    with mlir_mod_ctx() as ctx:
+
+        @device(dev)
+        def device_body():
+
+
+            transfers = 1
+
+            transfers_outer = transfers
+            transfers_inner = transfers
+
+            tranfer_size_elemnts = host_elements // transfers
+
+            eprint("[INFO] transfer size in KB: {}".format(tranfer_size_elemnts*4/1024))
+
+
+            #elements = 4096
+
+            tile_ty_size = 128
+
+            iters_outer = host_elements // tile_ty_size
+            iters_inner = host_elements // tile_ty_size
+
+
+            tile_ty = np.ndarray[(tile_ty_size,), np.dtype[np.int32]]
+
+            #buffer_ty = np.ndarray[(elements,), np.dtype[np.int32]]
+
+            data_ty = np.ndarray[(host_elements,), np.dtype[np.int32]]
+
+
+            # External, binary kernel definition
+            odd_even = external_func(
+                "odd_even",
+                inputs=[tile_ty, tile_ty,tile_ty, np.int32]
+            )
+
+            # Tile declarations
+            ShimTile00 = tile(0, 0)
+            #ShimTile10 = tile(1, 0)
+            ShimTile20 = tile(2, 0)
+            #MemTile01 = tile(0, 1)
+            #MemTile11 = tile(1, 1)
+            ComputeTile02 = tile(0, 2)
+            #ComputeTile12 = tile(1, 2)
+
+            # AIE-array data movement with object fifos
+            # Input
+            #of_in = object_fifo("in", ShimTile00, MemTile01, 2, tile_ty)
+            of_in1 = object_fifo("in1", ShimTile00, ComputeTile02, 2, tile_ty)
+            of_in_inner = object_fifo("in1_inner", ShimTile00, ComputeTile02, 2, tile_ty)
+            #object_fifo_link(of_in, of_in1)
+
+
+
+            # Output
+            #of_out1 = object_fifo("out1", ComputeTile02, MemTile01, 2, tile_ty)
+            of_out1 = object_fifo("out", ComputeTile02, ShimTile00, 2, tile_ty)
+            #object_fifo_link(of_out1, of_out)
+
+            #of_out1_odd = object_fifo("outodd", ComputeTile02, MemTile01, 2, tile_ty)
+            #of_out1_odd = object_fifo("odd", ComputeTile02, ShimTile00, 2, tile_ty)
+            #object_fifo_link(of_out1_odd, of_out_odd)
+
+
+
+
+
+            # Set up compute tiles
+            # Compute tile
+            @core(ComputeTile02, "odd_even.o")
+            def core_body_02():
+                # for _ in range_(0xFFFFFFFF):
+                #     for _ in range_(host_elements//elements):
+                #         for i in range_(iters):
+                #             elem_in = of_in1.acquire(ObjectFifoPort.Consume, 1)
+                #             elemOut_even = of_out1.acquire(ObjectFifoPort.Produce, 1)
+                #             elemOut_odd = of_out1_odd.acquire(ObjectFifoPort.Produce, 1)
+                #
+                #             for j in range_(tile_ty_size):
+                #               elemOut_even[j] = elem_in[j]
+                #               elemOut_odd[j] = elem_in[j]
+                #
+                #             of_in1.release(ObjectFifoPort.Consume, 1)
+                #             of_out1_odd.release(ObjectFifoPort.Produce, 1)
+                #             of_out1.release(ObjectFifoPort.Produce, 1)
+
+                for _ in range_(0xFFFFFFFF):
+                    #for _ in range_(iters):
+                    for _ in range_(iters_outer):
+                        elem_in = of_in1.acquire(ObjectFifoPort.Consume, 1)
+
+                        for _ in range_(iters_inner):
+                            elem_inner = of_in_inner.acquire(ObjectFifoPort.Consume, 1)
+                            out = of_out1.acquire(ObjectFifoPort.Produce, 1)
+
+                            call(odd_even, [elem_in, elem_inner, out, tile_ty_size])
+
+                            of_out1.release(ObjectFifoPort.Produce, 1)
+                            of_in_inner.release(ObjectFifoPort.Consume, 1)
+
+
+                        of_in1.release(ObjectFifoPort.Consume, 1)
+
+
+
+
+
+
+            tiles_to_trace = [ComputeTile02, ShimTile00]
+            if trace_size > 0:
+                trace_utils.configure_packet_tracing_flow(tiles_to_trace, ShimTile20)
+                #todo use other shimtile to trace?
+
+
+
+            # To/from AIE-array data movement
+            #d
+
+            #tiles_to_trace = [ComputeTile02, MemTile01, ShimTile00]
+            #if trace_size > 0:
+            #    trace_utils.configure_packet_tracing_flow(tiles_to_trace, ShimTile20)
+
+            @runtime_sequence(data_ty, data_ty,data_ty)
+            def sequence(inTensor,innerinTensor,outOddTensor,):
+
+                if trace_size > 0:
+                    trace_utils.configure_packet_tracing_aie2( #todo is this method correct form every npu?
+                        tiles_to_trace=tiles_to_trace,
+                        shim=ShimTile20,
+                        ddr_id=4,# 4 -> group_id(7)
+                        trace_size=trace_size,
+                    )
+
+
+
+                for i in range(transfers_outer):
+                    in_task = shim_dma_single_bd_task(of_in1, inTensor, offset= i *tranfer_size_elemnts ,sizes=[1, 1, 1, tranfer_size_elemnts],issue_token=False)
+                    dma_start_task(in_task)
+                    for i in range(transfers_inner):
+                        inner_in_task = shim_dma_single_bd_task(of_in_inner, innerinTensor, offset=i * tranfer_size_elemnts,
+                                                          sizes=[1, 1, 1, tranfer_size_elemnts], issue_token=False)
+                        out_task = shim_dma_single_bd_task(
+                            of_out1, outOddTensor, offset= i *tranfer_size_elemnts ,sizes=[1, 1, 1, tranfer_size_elemnts], issue_token=True
+                        )
+                        dma_start_task(inner_in_task, out_task, )
+                        dma_await_task(out_task)
+                        dma_free_task(inner_in_task)
+                    dma_free_task(in_task)
+
+                    #trace_utils.gen_trace_done_aie2(ShimTile20)
+
+
+
+
+
+    res = ctx.module.operation.verify()
+    if res == True:
+        print(ctx.module)
+    else:
+        print(res)
+
+
+external_mem_to_core()
---
+++ join_new/test.cpp
@@ -0,0 +1,381 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <set>
+
+#include "cxxopts.hpp"
+#include "test_utils.h"
+
+#include "xrt/xrt_bo.h"
+#include "xrt/xrt_device.h"
+#include "xrt/xrt_hw_context.h"
+#include "xrt/xrt_kernel.h"
+
+
+
+#include "xrt/xrt_graph.h"
+
+
+#ifndef DATATYPES_USING_DEFINED
+#define DATATYPES_USING_DEFINED
+using DATATYPE = std::uint32_t;
+#endif
+
+
+void my_init_xrt_load_kernel(xrt::device &device, xrt::kernel &kernel,
+                                      int verbosity, std::string xclbinFileName,
+                                      std::string kernelNameInXclbin) {
+  // Get a device handle
+  unsigned int device_index = 0;
+  device = xrt::device(device_index);
+
+  // Load the xclbin
+  if (verbosity >= 1)
+    std::cout << "Loading xclbin: " << xclbinFileName << "\n";
+  auto xclbin = xrt::xclbin(xclbinFileName);
+
+  if (verbosity >= 1)
+    std::cout << "Kernel opcode: " << kernelNameInXclbin << "\n";
+
+  // Get the kernel from the xclbin
+  auto xkernels = xclbin.get_kernels();
+  auto xkernel =
+      *std::find_if(xkernels.begin(), xkernels.end(),
+                    [kernelNameInXclbin, verbosity](xrt::xclbin::kernel &k) {
+                      auto name = k.get_name();
+                      if (verbosity >= 1) {
+                        std::cout << "Name: " << name << std::endl;
+                      }
+                      return name.rfind(kernelNameInXclbin, 0) == 0;
+                    });
+  auto kernelName = xkernel.get_name();
+  // Register xclbin
+  if (verbosity >= 1)
+    std::cout << "Registering xclbin: " << xclbinFileName << "\n";
+
+  device.register_xclbin(xclbin);
+
+  // Get a hardware context
+  if (verbosity >= 1)
+    std::cout << "Getting hardware context.\n";
+   //does set exclusive help?
+  xrt::hw_context context(device, xclbin.get_uuid(), xrt::hw_context::access_mode::exclusive);
+
+  // Get a kernel handle
+  if (verbosity >= 1)
+    std::cout << "Getting handle to kernel:" << kernelName << "\n";
+  kernel = xrt::kernel(context, kernelName);
+
+  return;
+}
+
+uint32_t getParity(uint32_t n) {
+  int count = 0;
+  while (n > 0) {
+    if (n & 1) { // Check if the least significant bit is 1
+      count++;
+    }
+    n >>= 1; // Right shift to check the next bit
+  }
+  return (count % 2 == 0) ? 0 : 1; // 0 for even parity, 1 for odd parity
+}
+
+uint32_t create_ctrl_pkt(int operation, int beats, int addr,
+                         int ctrl_pkt_read_id = 28) {
+  uint32_t ctrl_pkt = ((ctrl_pkt_read_id & 0xFF) << 24) |
+                      ((operation & 0x3) << 22) | ((beats & 0x3) << 20) |
+                      (addr & 0x7FFFF);
+  ctrl_pkt |= (0x1 ^ getParity(ctrl_pkt)) << 31;
+  return ctrl_pkt;
+}
+
+int main(int argc, const char *argv[]) {
+  // Program arguments parsing
+  cxxopts::Options options("odd_even Kernel");
+  test_utils::add_default_options(options);
+  options.add_option("","e","host_elements", "host elements (assumed to be 4 bytes)",
+      cxxopts::value<int64_t>()->default_value("1024"),"host elements");
+
+  cxxopts::ParseResult vm;
+  test_utils::parse_options(argc, argv, options, vm);
+  int verbosity = vm["verbosity"].as<int>();
+  int n_iterations = vm["iters"].as<int>();
+  int n_warmup_iterations = vm["warmup"].as<int>();
+  int trace_size = vm["trace_sz"].as<int>();
+  std::string trace_file = vm["trace_file"].as<std::string>();
+
+  // Declaring design constants
+  constexpr bool VERIFY = false;
+  constexpr bool PRINT_OUT_BUFFERS = true;
+  //constexpr int64_t oneMBElements = 2*128*1024;
+  //not quite one GB 128MB otherwise timeout happens
+  //constexpr int64_t oneGBElements =  2048 * oneMBElements;
+  int64_t host_elements = vm["host_elements"].as<int64_t>();
+   std::cout << "host_elements: " << host_elements << "\n";
+  int64_t IN_SIZE = host_elements;
+  int64_t OUT_SIZE = IN_SIZE *IN_SIZE;
+  bool enable_ctrl_pkts = false;
+
+
+  // Load instruction sequence
+  std::vector<uint32_t> instr_v =
+      test_utils::load_instr_binary(vm["instr"].as<std::string>());
+
+  if (verbosity >= 1)
+    std::cout << "Sequence instr count: " << instr_v.size() << "\n";
+
+  // Start the XRT context and load the kernel
+  xrt::device device;
+  xrt::kernel kernel;
+
+  /*test_utils::init_xrt_load_kernel(device, kernel, verbosity,
+                                   vm["xclbin"].as<std::string>(),
+                                   vm["kernel"].as<std::string>());*/
+
+  my_init_xrt_load_kernel(device, kernel, verbosity,
+                                   vm["xclbin"].as<std::string>(),
+                                   vm["kernel"].as<std::string>());
+
+
+  // set up the buffer objects
+  auto bo_instr = xrt::bo(device, instr_v.size() * sizeof(int),
+                          XCL_BO_FLAGS_CACHEABLE, kernel.group_id(1));
+  auto bo_inA = xrt::bo(device, IN_SIZE * sizeof(DATATYPE),
+                        XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(3));
+
+  auto bo_inB = xrt::bo(device, OUT_SIZE * sizeof(DATATYPE),
+                         XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(4));
+  auto bo_outC = xrt::bo(device, OUT_SIZE * sizeof(DATATYPE),
+                         XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(5));
+
+  // If we enable control packets, then this is the input xrt buffer for that.
+  // Otherwise, this is a dummy placedholder buffer.
+    //todo why do we need this?
+  auto bo_ctrlpkts =
+      xrt::bo(device, 8, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(6));
+
+  // Workaround so we declare a really small trace buffer when one is not used
+  // Second workaround for driver issue. Allocate large trace buffer *4
+  // This includes the 8 bytes needed for control packet response.
+  //todo why 4* because of a segfault in the driver?
+  int tmp_trace_size = (trace_size > 0) ? trace_size * 4  : 1;
+  auto bo_trace = xrt::bo(device, tmp_trace_size, XRT_BO_FLAGS_HOST_ONLY,
+                          kernel.group_id(7));
+
+  if (verbosity >= 1)
+    std::cout << "Writing data into buffer objects.\n";
+
+  // Copy instruction stream to xrt buffer object
+  void *bufInstr = bo_instr.map<void *>();
+  memcpy(bufInstr, instr_v.data(), instr_v.size() * sizeof(int));
+
+  // Initialize buffer bo_inA
+
+  DATATYPE *bufInA = bo_inA.map<DATATYPE *>();
+  memset(bufInA, 0, IN_SIZE * sizeof(DATATYPE));
+
+   DATATYPE *bufInB = bo_inB.map<DATATYPE *>();
+  memset(bufInB, 0, IN_SIZE * sizeof(DATATYPE));
+
+  // Zero out buffer bo_outC
+  DATATYPE *bufOut = bo_outC.map<DATATYPE *>();
+  memset(bufOut, 0, OUT_SIZE * sizeof(DATATYPE));
+
+
+
+  char *bufTrace = bo_trace.map<char *>();
+  uint32_t *bufCtrlPkts = bo_ctrlpkts.map<uint32_t *>();
+
+
+    // Set control packet values
+  if (trace_size > 0 && enable_ctrl_pkts) {
+    bufCtrlPkts[0] = create_ctrl_pkt(1, 0, 0x32004); // core status
+    bufCtrlPkts[1] = create_ctrl_pkt(1, 0, 0x320D8); // trace status
+    if (verbosity >= 1) {
+      std::cout << "bufCtrlPkts[0]:" << std::hex << bufCtrlPkts[0] << std::endl;
+      std::cout << "bufCtrlPkts[1]:" << std::hex << bufCtrlPkts[1] << std::endl;
+    }
+  }
+
+  // sync host to device memories
+  bo_instr.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_inA.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_outC.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_inB.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+  if (trace_size > 0) {
+    bo_trace.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+    if (enable_ctrl_pkts)
+      bo_ctrlpkts.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  }
+
+
+
+  // Execute the kernel and wait to finish
+  int errors = 0;
+  unsigned num_iter = n_iterations + n_warmup_iterations;
+  float npu_time_total = 0;
+  float npu_time_min = 9999999;
+  float npu_time_max = 0;
+
+
+  unsigned int opcode = 3;
+
+  /*auto run = xrt::run(kernel);
+  run.set_arg(0,opcode);
+  run.set_arg(1,bo_instr);
+  run.set_arg(2,instr_v.size());
+
+  run.set_arg(3,bo_inA);
+  run.set_arg(4,bo_outC);
+  run.set_arg(5,bo_outOdd);
+  //not sure about this one
+  run.set_arg(6,bo_ctrlpkts);
+  run.set_arg(7,bo_trace);*/
+
+
+  for (int iter = 0; iter < num_iter; iter++) {
+    //todo put back warmup iterrations
+     std::cout << "iter: " << iter <<"\n";
+
+      if (verbosity >= 1) {
+      std::cout << "Setting inputs and zero out out buffers ..." << std::endl;
+    }
+
+
+      for (int64_t i = 0; i < IN_SIZE; i++)
+        bufInA[i] = i + iter +1; //plus one for first iteration
+
+       for (int64_t i = 0; i < IN_SIZE; i++)
+        bufInB[i] = i + iter +1; //plus one for first iteration
+
+      // Zero out buffer bo_outC
+      memset(bufOut, 0, OUT_SIZE * sizeof(DATATYPE));
+
+      if (trace_size > 0 ) {
+          //zero out buffTrace each iteration???
+          memset(bufTrace,0,tmp_trace_size*sizeof(char));
+          bo_trace.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+      }
+      //this should not be needed
+      //bo_instr.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+      bo_inA.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+      bo_inB.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+      bo_outC.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+
+    std::cout << "Running Kernel.\n";
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    auto run =
+        kernel(opcode, bo_instr, instr_v.size(), bo_inA,bo_inB, bo_outC, bo_ctrlpkts, bo_trace);
+
+
+        //xrt::autostart its {};
+        //its.iterations = 0;
+
+    //run.start();
+
+
+    ert_cmd_state r = run.wait();
+    //run.wait2();
+    auto stop = std::chrono::high_resolution_clock::now();
+    run.abort();
+     if(r != ERT_CMD_STATE_COMPLETED){
+        std::cout << "run.wait() did not return ERT_CMD_STATE_COMPLETED: " << r<<"\n";
+    }
+
+    // Sync device to host memories
+    bo_outC.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+
+    if (trace_size > 0)
+      bo_trace.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+
+    //todo should tmp_trace_size be used here?
+    if (trace_size > 0 ) {
+      test_utils::write_out_trace(((char *)bufTrace), trace_size,
+                                  trace_file);
+    }
+    // Accumulate run times
+    /* Warmup iterations do not count towards average runtime. */
+
+    float npu_time =
+        std::chrono::duration_cast<std::chrono::microseconds>(stop - start)
+            .count();
+    std::cout << ""
+              << "NPU time: " << npu_time << "us."
+              << std::endl;
+    npu_time_total += npu_time;
+    npu_time_min = (npu_time < npu_time_min) ? npu_time : npu_time_min;
+    npu_time_max = (npu_time > npu_time_max) ? npu_time : npu_time_max;
+
+
+
+     if (PRINT_OUT_BUFFERS >= 1) {
+      std::cout << "Join:" << std::endl;
+
+      for (uint32_t i = 0; i < OUT_SIZE; i++) {
+      int32_t test = bufOut[i];
+      std::cout << test << " ";
+    }
+
+    }
+
+    // Compare out to golden
+
+    if(VERIFY){
+        if (verbosity >= 1) {
+            std::cout << "Verifying results ..." << std::endl;
+        }
+        int cnt_odd_in = 0;
+        int cnt_even_in = 0;
+        int cnt_odd = 0;
+        int cnt_even =0;
+
+
+
+
+
+
+
+
+         //if (verbosity >= 1) {
+
+
+        //}
+    }
+
+
+
+
+  }
+
+  // print out profiling result
+  std::cout << std::endl
+          << "Number of iterations: " << n_iterations
+          << " (warmup iterations: " << n_warmup_iterations << ")"
+          << std::endl;
+
+  std::cout << std::endl
+            << "Avg NPU time: " << npu_time_total / n_iterations << "us."
+            << std::endl;
+
+
+
+
+  // Print Pass/Fail result of our test
+  if (!errors) {
+    std::cout << std::endl << "PASS!" << std::endl << std::endl;
+    return 0;
+  } else {
+    std::cout << std::endl
+              << errors << " mismatches." << std::endl
+              << std::endl;
+    std::cout << std::endl << "fail." << std::endl << std::endl;
+    return 1;
+  }
+}
